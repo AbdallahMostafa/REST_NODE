9bc74a1dc9912630aebe30eb6497761f3dd61e92
AbdallahMostafa <abdallahmostafa377@gmail.com>
2022-07-27 23:04:49 +0200

    using async/wait

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,6 +6,29 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 const User = require('../models/user');
 
+
+// async code example 
+// exports.getPosts = async (req, res, next) => {
+//     const currentPage = req.query.page || 1;
+//     const perPage =  2;
+//     let totalItems;
+//     try {
+//     const totalItems = await Post.find().countDocuments()
+//     const posts = await Post.find()
+//         .skip((currentPage - 1) * perPage)
+//         .limit(perPage);
+//     res.status(200).json({
+//         message: 'posts fetched successfully', 
+//         posts: posts,  
+//         totalItems: totalItems 
+//     })
+//     } catch (err){
+//         if(!err.statusCode) {
+//             err.statusCode = 500;
+//         }
+//         next(err);
+//     }    
+// };
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage =  2;
